7e0026417dd3f43959a1563f5545cebf3d955322
Bastian Wagner <49192490+wagnerbastian@users.noreply.github.com>
2021-10-12 21:40:56 +0200

    logging angepasst

--- src/util/logger.service.ts
+++ src/util/logger.service.ts
@@ -36,7 +36,7 @@ export class Logger {
             fs.mkdirSync('logs');
         } catch(e) {}
         fs.writeFile("results.json", JSON.stringify(text), function(){});
-        fs.writeFile("../masterarbeit-visualisierung/src/app/results.json", JSON.stringify(text), function(){});
+        // fs.writeFile("../masterarbeit-visualisierung/src/app/results.json", JSON.stringify(text), function(){});
         
 
         if (parameters.extendedLogging) {
@@ -69,7 +69,7 @@ export class Logger {
             fs.mkdirSync('logs');
         } catch(e) {}
         fs.writeFile("run.json", JSON.stringify(text), function(){})
-        fs.writeFile("../masterarbeit-visualisierung/src/app/run.json", JSON.stringify(text), function(){})
+        // fs.writeFile("../masterarbeit-visualisierung/src/app/run.json", JSON.stringify(text), function(){})
 
         if (parameters.extendedLogging) {
             fs.writeFile(`./logs/run-${start.split(':').join('-')}.json`, JSON.stringify(text), function(){});
